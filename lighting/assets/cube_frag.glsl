f14895e942a2ced937171a3c648f6c0dff79118c
rockssalt05 <rockssalt05@gmail.com>
2022-06-28 21:34:59 +0300

    renamed a variable

--- lighting/assets/cube_frag.glsl
+++ lighting/assets/cube_frag.glsl
@@ -24,21 +24,21 @@ uniform Material material;
 uniform Light light;
 
 void main() {
-	vec3 mDiffuse = vec3(texture(material.diffuse, texCoord));
-	vec3 mSpecular = vec3(texture(material.specular, texCoord));
+	vec3 materialDiffuse = vec3(texture(material.diffuse, texCoord));
+	vec3 materialSpecular = vec3(texture(material.specular, texCoord));
 
-	vec3 ambient = light.ambient * mDiffuse;
+	vec3 ambient = light.ambient * materialDiffuse;
 
 	vec3 lightDir = normalize(light.pos - fragPos);
 	vec3 norm = normalize(normal);
 	vec3 diffuse = max(dot(norm, lightDir), 0.0)
-		* light.diffuse * mDiffuse;
+		* light.diffuse * materialDiffuse;
 
 	vec3 viewDir = normalize(viewPos - fragPos);
 	vec3 reflectDir = reflect(-lightDir, norm);
 	vec3 specular = pow(max(dot(
 		viewDir, reflectDir), 0.0), material.shininess)
-		* light.specular * mSpecular;
+		* light.specular * materialSpecular;
 
 	fragColour = vec4(ambient + diffuse + specular, 1.0);
 }
